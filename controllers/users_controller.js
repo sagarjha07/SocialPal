60ee9ab3c086c97a8150dad1d9e1d113bdd90946
Sagar kumar jha <jha8768@gmail.com>
2021-05-17 10:54:18 +0530

    Login and Signup form added

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,21 @@
 module.exports.profile = (req, res) => {
 	res.render("user_profile",{title:"Profile"});
 };
+
+module.exports.signUp=(req,res)=>{
+	return res.render("signup", { title: "SignUp" });
+}
+
+module.exports.signIn=(req,res)=>{
+	return res.render("signin", { title: "Login" });
+}
+
+module.exports.create=(req,res)=>{
+	
+};
+
+module.exports.createSession=(req,res)=>{
+
+};
+
+
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const userController = require("../controllers/users_controller");
 
 router.get("/profile", userController.profile);
+router.get("/signin",userController.signIn);
+router.get("/signup",userController.signUp);
+
+router.post("/create",userController.create);
+router.post("/create-session",userController.createSession);
 
 module.exports = router;
