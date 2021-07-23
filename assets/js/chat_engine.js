d8650aa9fd5c75f3f73dc54f2c4258c25b451479
Sagar kumar jha <jha8768@gmail.com>
2021-07-23 10:37:30 +0530

    Chatting engine modifications

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,72 +1,75 @@
-class ChatEngine{
-    constructor(chatBoxId, userEmail){
-        this.chatBox = $(`#${chatBoxId}`);
-        this.userEmail = userEmail;
+class ChatEngine {
+	constructor(chatBoxId, userEmail) {
+		this.chatBox = $(`#${chatBoxId}`);
+		this.userEmail = userEmail;
 
-        this.socket = io.connect('http://localhost:8000');
+		this.socket = io.connect("http://localhost:8000");
 
-        if (this.userEmail){
-            this.connectionHandler();
-        }
+		if (this.userEmail) {
+			this.connectionHandler();
+		}
+	}
 
-    }
+	connectionHandler() {
+		let self = this;
 
+		this.socket.on("connect", function () {
+			console.log("connection established using sockets...!");
 
-    connectionHandler(){
-        let self = this;
+			self.socket.emit("join_room", {
+				user_email: self.userEmail,
+				chatroom: "socialPal",
+			});
 
-        this.socket.on('connect', function(){
-            console.log('connection established using sockets...!');
+			self.socket.on("user_joined", function (data) {
+				console.log("a user joined!", data);
+			});
+		});
 
+		$("#send-message").on("keypress click", function (e) {
+			// e.preventdefault();
+			let msg = $("#chat-message-input").val();
+			console.log(msg);
+			if (e.which === 13 || e.type === "click") {
+				if (msg != "") {
+					self.socket.emit("send_message", {
+						message: msg,
+						user_email: self.userEmail,
+						chatroom: "socialPal",
+					});
+				}
+				document.getElementById("chat-message-input").value = "";
+			}
+		});
 
-            self.socket.emit('join_room', {
-                user_email: self.userEmail,
-                chatroom: 'socialPal'
-            });
+		self.socket.on("receive_message", function (data) {
+			console.log("message received", data.message);
 
-            self.socket.on('user_joined', function(data){
-                console.log('a user joined!', data);
-            })
+			let newMessage = $("<li>");
 
+			let messageType = "other-message";
 
-        });
+			if (data.user_email === self.userEmail) {
+				messageType = "self-message";
+			}
 
-        // CHANGE :: send a message on clicking the send message button
-        $('#send-message').click(function(){
-            let msg = $('#chat-message-input').val();
-            console.log(msg);
-            if (msg != ''){
-                self.socket.emit('send_message', {
-                    message: msg,
-                    user_email: self.userEmail,
-                    chatroom: 'socialPal'
-                });
-            }
-        });
+			newMessage.append(
+				$("<span>", {
+					html: data.message,
+				})
+			);
 
-        self.socket.on('receive_message', function(data){
-            console.log('message received', data.message);
+			newMessage.append(
+				$("<sub>", {
+					html: data.user_email,
+				})
+			);
 
+			newMessage.addClass(messageType);
 
-            let newMessage = $('<li>');
-
-            let messageType = 'other-message';
-
-            if (data.user_email === self.userEmail){
-                messageType = 'self-message';
-            }
-
-            newMessage.append($('<span>', {
-                'html': data.message
-            }));
-
-            newMessage.append($('<sub>', {
-                'html': data.user_email
-            }));
-
-            newMessage.addClass(messageType);
-
-            $('#chat-messages-list').append(newMessage);
-        })
-    }
+			$("#chat-messages-list").append(newMessage);
+			var container=document.getElementById("chat-messages-list");
+			container.scrollTop=container.scrollHeight;
+		});
+	}
 }
